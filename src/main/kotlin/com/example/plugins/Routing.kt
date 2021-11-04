124351c7be111f4cb85352f8d5d65b35691938ae
Jhoan Gamarra <jhoangamarraurzola@gmail.com>
2021-11-03 22:34:35 -0500

    test gitignore

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,20 +1,15 @@
 package com.example.plugins
 
-import com.example.routes.users
 import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
 
 fun Application.configureRouting() {
 
     routing {
-        users()
         get("/") {
             call.respondText("Hello World!")
         }
     }
-
 
 }
